b71e3952a9a6fd215b2c6a6e739a7b205f60eb7a
Gilbert <gyim@hawaii.edu>
2015-09-22 13:55:00 -1000

    In Class Practice WOD 9/22/2015
    Finished rest of functions

--- campaign.js
+++ campaign.js
@@ -23,10 +23,26 @@ console.log(contributionsByParty(tempdata));
 function partyContributionObjects(data){
     var contributions = contributionsByParty(data);
     return _.map(_.keys(contributions),function(key){
-            return {name: key, amount: contributions[key]};
+            return {party: key, amount: contributions[key]};
     });
 }
 function maxContributionsToParty(data){
     return _.max(partyContributionObjects(data), function(record){return record.amount;});
 }
 console.log(maxContributionsToParty(tempdata));
+
+function noncandidate(data){
+    return _.groupBy(data, "Noncandidate Committee Name");
+}
+function contributed(data){
+    return _.mapObject(noncandidate(data), function(value,key){
+        return _.reduce(value, add, 0);
+    });
+}
+function contributorTotals(data){
+    var contributors = contributed(data)
+    return _.map(_.keys(contributors), function(key){
+        return {contributor: key, amount: contributors[key]};
+    });
+}
+console.log(contributorTotals(tempdata));
